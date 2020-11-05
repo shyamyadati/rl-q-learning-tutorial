ed2b48b800a0b6b3563f3bae42cd64cfac42c68f
Shyam Yadati <shyam@fringeai.com>
2020-11-05 14:51:37 -0500

    added q-learning simulation

--- tutorial.py
+++ tutorial.py
@@ -9,10 +9,12 @@ NOTE:
 # =============================================================================
 # standard library imports
 import time
+import random
 
 # -------------------------------------
 # third party imports
 import gym  # OpenAI Gym
+import numpy as np
 
 # -------------------------------------
 # local imports
@@ -71,9 +73,25 @@ def print_rewards_table(env):
             print(f"        done        : {done}")
 
 # -------------------------------------
+def replay(env, frames, time_step=0.01):
+    """
+    replays a simulation given the env, and frames captured during the simulation
+    """
+    print("Replaying simulation:\n")
+    env.s = 328
+    env.render()
+    for f in frames:
+        time.sleep(time_step)
+        for i in range(8):
+            print ("\033[A\033[A") # clear one line from stdout
+        env.s = f['state']
+        env.render()
+
+# -------------------------------------
 def run_random_sim(env):
     """
-    Description: Runs a random simulation given the environment
+    Description:
+        Runs a random simulation given the environment
     """
     env.s = 328  # set environment to illustration's state
 
@@ -111,19 +129,125 @@ def run_random_sim(env):
     return epochs, penalties, frames
 
 # -------------------------------------
-def replay(env, frames):
+def train_q_learning_agent(env, num_epochs=100000):
     """
-    replays a simulation given the env, and frames captured during the simulation
+    Description:
+        train_agent will train the Q-learning agent 
+    Returns:
+        the q_table for the agent
     """
-    print("Replaying simulation:\n")
-    env.s = 328
-    env.render()
-    for f in frames:
-        time.sleep(0.01)
-        for i in range(8):
-            print ("\033[A\033[A") # clear one line from stdout
-        env.s = f['state']
-        env.render()
+    # create the q_table size of the observation_space (state space) x action_space
+    print("Training the Q-learning agent")
+    print(f"Creating q_table of ({env.observation_space.n} x {env.action_space.n})")
+    q_table = np.zeros([env.observation_space.n, env.action_space.n])
+
+    # set up the hyperparameters for the Q-learning model
+    alpha = 0.1
+    gamma = 0.6
+    epsilon = 0.1
+
+    # setup training variables
+    all_epochs = []
+    all_penalties = []
+
+    print("Staring training...")
+    start_time = time.time()
+    for i in range(num_epochs):
+        # at the beginning of each epoch - start with a new random state
+        state = env.reset()
+
+        # reset the epoch's loop variables
+        epochs, penalties, reward, = 0, 0, 0
+        done = False
+    
+        while not done:
+            # get an action - either a random one depending on epsilon, or 
+            # one from the learned values
+            if random.uniform(0, 1) < epsilon:
+                action = env.action_space.sample() # Explore action space
+            else:
+                action = np.argmax(q_table[state]) # Exploit learned values
+
+            # apply the selected action to the environment
+            next_state, reward, done, info = env.step(action) 
+        
+            # get the old value from the table
+            old_value = q_table[state, action]   
+
+            # get the max predicted award given the next state
+            next_max = np.max(q_table[next_state])   
+
+            # Update the table
+            new_value = (1 - alpha) * old_value + alpha * (reward + gamma * next_max)
+            q_table[state, action] = new_value
+
+            if reward == -10:
+                penalties += 1
+
+            state = next_state
+            epochs += 1
+        if i%100 == 0:
+            if i != 0:
+                print ("\033[A\033[A") # clear one line from stdout
+            print(f"    Episode: {i}")
+
+    print ("\033[A\033[A") # clear one line from stdout
+    print(f"    Episode: {num_epochs}")
+    print(f"Training finished - elapsed time: {(time.time() - start_time):0.3f} s")
+    return q_table
+
+# -------------------------------------
+def run_sim(env, q_table=None):
+    """
+    Description:
+        Runs a random simulation given the environment
+    """
+    env.s = 328  # set environment to illustration's state
+
+    # initalize the simlation variables
+    epochs = 0
+    penalties, reward = 0, 0
+    frames = [] # for animation
+    done = False
+
+    # run through the simulation
+    print("Running the simulation ...")
+    while not done:
+        if q_table is None: 
+            # select a random action
+            action = env.action_space.sample() 
+        else:
+            #else, if the agent was provided us it
+            action = np.argmax(q_table[env.s])
+           
+        # Apply the selected action to the environment
+        state, reward, done, info = env.step(action)
+
+        # count the number of penalties that we incurr
+        if reward == -10:
+            penalties += 1
+        
+        # Put each rendered frame into dict for animation after
+        frames.append({
+            'frame': env.render(mode='ansi'),
+            'state': state,
+            'action': action,
+            'reward': reward
+            }
+        )
+
+        # increment the epoc count
+        epochs += 1
+    
+    return epochs, penalties, frames
+
+# -------------------------------------
+def run_q_learning_sim(env, q_table):
+    """
+    Description:
+        Runs the simulation
+    """
+    pass
 
 # =============================================================================
 # start main
@@ -176,6 +300,17 @@ if __name__ == "__main__":
     print(f"Penalties incurred : {penalties}")
     replay(env, frames)
     input("\nPress enter to continue")
+
+    # ---------------------------------
+    header("Solve the problem using Q-Learning")
+    q_table = train_q_learning_agent(env)
+    epochs, penalties, frames = run_sim(env, q_table)
+    print(f"Timesteps taken    : {epochs}")
+    print(f"Penalties incurred : {penalties}")
+    time.sleep(0.5)
+    replay(env, frames, time_step=0.25)
+    input("\nPress enter to continue")
+
 
 
 
